925eaf8fc17b396fe8943adf70828a86ad4ecf4d
Denilson <denilsonsobreira0@gmail.com>
2023-06-29 16:04:35 -0300

    Fisrt Commit

---
+++ bin/PostoDFP.dpr
@@ -0,0 +1,14 @@
+program PostoDFP;
+
+uses
+  Vcl.Forms,
+  uFrmPrincipal in 'view\uFrmPrincipal.pas' {FrmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFrmPrincipal, FrmPrincipal);
+  Application.Run;
+end.
