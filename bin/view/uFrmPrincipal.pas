dce4d097c4e756599d1f47f9e8733e21d6e761c0
Denilson <denilsonsobreira0@gmail.com>
2023-06-29 18:47:09 -0300

    Adicionando botões e imagens a tela inicial

--- bin/view/uFrmPrincipal.pas
+++ bin/view/uFrmPrincipal.pas
@@ -4,10 +4,19 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus, Vcl.StdCtrls, Vcl.ComCtrls,
+  Vcl.ExtCtrls, Vcl.ImgList, Vcl.Buttons;
 
 type
   TFrmPrincipal = class(TForm)
+    PnlPrincipal: TPanel;
+    MainMenu: TMainMenu;
+    MenuCadastros: TMenuItem;
+    MItemTanques: TMenuItem;
+    MItemBombas: TMenuItem;
+    btnValores: TBitBtn;
+    btnAbastecer: TBitBtn;
+    btnRelatorio: TBitBtn;
   private
     { Private declarations }
   public
